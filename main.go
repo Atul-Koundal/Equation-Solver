efe3d81441e11b3a7bcdbf0e5f25d652dddfe655
Atul-Koundal <24bcs031@nith.ac.in>
2026-06-08 15:33:58 +0530

    Defining the token strucure to wowk eith

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
 
-func main() {
-	fmt.Println("This would be a equation solver operating via AST")
+//So the main logic is i will tokenise the equation
+
+type TokenType int
+
+const (
+	TokenType_Unknown TokenType = iota
+	TokenType_Number
+	TokenType_Operator
+	TokenType_OpenBracket
+	TokenType_CloseBracket
+)
+
+type Token struct {
+	Val  string
+	Type TokenType
 }
+
