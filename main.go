a2ae20c288f57216374add7ee9c38b99b12fa868
Atul-Koundal <24bcs031@nith.ac.in>
2026-06-08 15:56:04 +0530

    Implemnted a function to tokenise the string input

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-
+import (
+	"unicode"
+)
 
 //So the main logic is i will tokenise the equation then i will build the ast from this to 
 //deal with the predence and all ...
@@ -26,4 +28,53 @@ const (
 type Token struct {
 	Type    TokenType
 	Literal string
+}
+
+
+func tokenize(input string) []Token {
+	var tokens []Token
+
+	for i := 0; i < len(input); {
+		ch := rune(input[i])
+
+		if unicode.IsSpace(ch) {
+			i++
+			continue
+		}
+
+		if unicode.IsDigit(ch) {
+			start := i
+
+			for i < len(input) && unicode.IsDigit(rune(input[i])) {
+				i++
+			}
+
+			tokens = append(tokens, Token{
+				Type:    NUMBER,
+				Literal: input[start:i],
+			})
+			continue
+		}
+		switch ch {
+		case '+':
+			tokens = append(tokens, Token{Type: PLUS, Literal: "+"})
+		case '-':
+			tokens = append(tokens, Token{Type: MINUS, Literal: "-"})
+		case '*':
+			tokens = append(tokens, Token{Type: MUL, Literal: "*"})
+		case '/':
+			tokens = append(tokens, Token{Type: DIV, Literal: "/"})
+		case '(':
+			tokens = append(tokens, Token{Type: LPAREN, Literal: "("})
+		case ')':
+			tokens = append(tokens, Token{Type: RPAREN, Literal: ")"})
+		default:
+			panic("invalid character: " + string(ch))
+		}
+
+		i++
+	}
+
+	tokens = append(tokens, Token{Type: EOF})
+	return tokens
 }
