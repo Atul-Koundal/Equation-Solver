8f7fe04809c1e37f4048e3302eb0e12f67a22cdb
Atul-Koundal <24bcs031@nith.ac.in>
2026-06-09 21:22:29 +0530

    Simple structure is done

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
 	"unicode"
 )
-
-//So the main logic is i will tokenise the equation then i will build the ast from this to 
-//deal with the predence and all ...
-//Basically the replication on how interpreter and compilers work on these equations
 
 type TokenType string
 
@@ -24,12 +22,10 @@ const (
 	EOF TokenType = "EOF"
 )
 
-//This Literal just meant what is the value of the particular token
 type Token struct {
 	Type    TokenType
 	Literal string
 }
-
 
 func tokenize(input string) []Token {
 	var tokens []Token
@@ -55,6 +51,7 @@ func tokenize(input string) []Token {
 			})
 			continue
 		}
+
 		switch ch {
 		case '+':
 			tokens = append(tokens, Token{Type: PLUS, Literal: "+"})
@@ -77,4 +74,150 @@ func tokenize(input string) []Token {
 
 	tokens = append(tokens, Token{Type: EOF})
 	return tokens
+}
+
+type Node interface{}
+
+type NumberNode struct {
+	Value float64
+}
+
+type BinaryNode struct {
+	Left  Node
+	Op    TokenType
+	Right Node
+}
+
+type Parser struct {
+	tokens []Token
+	pos    int
+}
+
+func NewParser(tokens []Token) *Parser {
+	return &Parser{
+		tokens: tokens,
+	}
+}
+
+func (p *Parser) current() Token {
+	return p.tokens[p.pos]
+}
+
+func (p *Parser) eat(t TokenType) {
+	if p.current().Type != t {
+		panic(fmt.Sprintf(
+			"expected %s got %s",
+			t,
+			p.current().Type,
+		))
+	}
+	p.pos++
+}
+
+func (p *Parser) parse() Node {
+	return p.expression()
+}
+
+func (p *Parser) expression() Node {
+	node := p.term()
+
+	for p.current().Type == PLUS || p.current().Type == MINUS {
+		op := p.current().Type
+		p.pos++
+
+		node = &BinaryNode{
+			Left:  node,
+			Op:    op,
+			Right: p.term(),
+		}
+	}
+
+	return node
+}
+
+func (p *Parser) term() Node {
+	node := p.factor()
+
+	for p.current().Type == MUL || p.current().Type == DIV {
+		op := p.current().Type
+		p.pos++
+
+		node = &BinaryNode{
+			Left:  node,
+			Op:    op,
+			Right: p.factor(),
+		}
+	}
+
+	return node
+}
+
+func (p *Parser) factor() Node {
+	token := p.current()
+
+	switch token.Type {
+
+	case NUMBER:
+		p.eat(NUMBER)
+
+		val, _ := strconv.ParseFloat(token.Literal, 64)
+
+		return &NumberNode{
+			Value: val,
+		}
+
+	case LPAREN:
+		p.eat(LPAREN)
+
+		node := p.expression()
+
+		p.eat(RPAREN)
+
+		return node
+	}
+
+	panic("unexpected token")
+}
+
+func evaluate(node Node) float64 {
+	switch n := node.(type) {
+
+	case *NumberNode:
+		return n.Value
+
+	case *BinaryNode:
+		left := evaluate(n.Left)
+		right := evaluate(n.Right)
+
+		switch n.Op {
+		case PLUS:
+			return left + right
+
+		case MINUS:
+			return left - right
+
+		case MUL:
+			return left * right
+
+		case DIV:
+			return left / right
+		}
+	}
+
+	panic("invalid node")
+}
+
+func main() {
+	input := "(2 + 3) * 4 - 10 / 2"
+
+	tokens := tokenize(input)
+
+	parser := NewParser(tokens)
+
+	ast := parser.parse()
+
+	result := evaluate(ast)
+
+	fmt.Printf("Expression: %s\n", input)
+	fmt.Printf("Result: %.2f\n", result)
 }
